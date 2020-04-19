4ffc604bcae3e67e29e0352963edcb82e8ef2a49
azmpse <60586550+azmpse@users.noreply.github.com>
2020-04-18 22:46:57 -0700

    Delete forms.py

--- forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-
-class LoginForm(FlaskForm):
-    username = StringField('Set Username', validators=[DataRequired()])
-    password = PasswordField('Set Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remeber Me')
-    submit = SubmitField('Sign On')
