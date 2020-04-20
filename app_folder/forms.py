42a7a763d5fbf44e0f6fdea580facfd6028ba9e9
azmpse <60586550+azmpse@users.noreply.github.com>
2020-04-19 19:35:01 -0700

    docstring

--- app_folder/forms.py
+++ app_folder/forms.py
@@ -3,6 +3,16 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
  
 class LoginForm(FlaskForm):
+    """ Create form for Login page
+    Allow to set reference names for triggers and check validaties for inputs.
+    
+    Parameters:
+    FlaskForm: make sure reference names are matching
+    
+    4/19 Ali
+    
+    """
+ 
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
