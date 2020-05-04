8b68eaf6581d16d46e09fe6c1da77238cee0bea3
dkiskamp <47258708+dkiskamp@users.noreply.github.com>
2020-05-03 20:38:06 -0700

    Cleaned up some 404s

--- app_folder/routes.py
+++ app_folder/routes.py
@@ -17,44 +17,52 @@ def index():
     """
     Returns the rendered 'index.html' template.
 
-    Parameters: 
+    Parameters:
         none
 
-    Returns: 
+    Returns:
         render_template('index.html'): the rendered version of the index HTML page, which has the website title,
         a link to the login page, and a link to the create-account page
 
     Changelog:
         Saima 4/19: created homepage in index.html
     """
-    return render_template('index.html') 
+    return render_template('index.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     """
     Create address for Login page
-    
-    Returns to: 
+
+    Returns to:
         login.html.
         forms.py for LoginForm class.
         Sign In
-    
+
     Changelog:
         4/19 Ali
-        
+
         Dylan 4/19: Added session username, changed flash message
 
         Dylan 5/3: session['username'] receiving different data to fix a bug
-    
+
     """
     current_form = LoginForm()
     if current_form.validate_on_submit():
-        session['username'] = current_form.username.data
-        flash(f'Hello {current_form.username.data}!')
-        return redirect('/')
+        user = User.query.filter_by(
+            username=current_form.username.data).first()
+        if user == None:
+            flash('Incorrect username or password')
+        elif current_form.password.data == user.password_hash:
+            session['username'] = user.username
+            flash(f'Hello {user.username}!')
+            return redirect(url_for('accountHomePage'))
+            exit
+        else:
+            flash('Incorrect username or password')
+        
     return render_template('login.html', title='Sign In', form=current_form)
-
 
 
 @app.route("/create-account", methods=['GET', 'POST'])
@@ -70,11 +78,19 @@ def createaccount():
     '''
     form = RegisterForm()
     if form.validate_on_submit():
-        u = User(username=form.username.data, email=form.email.data, password_hash = form.password.data)
-        db.session.add(u)
-        db.session.commit()
-        flash('You have created an account!')
-        return redirect('/meetings')
+        user = User(username=form.username.data, email=form.email.data,
+                 password_hash=form.password.data)
+        if User.query.filter_by(email=user.email).first() is not None:
+            flash('Email already exists')
+            return redirect(url_for('createaccount'))
+        elif User.query.filter_by(username=user.username).first() is not None:
+            flash('User name already exists')
+            return redirect(url_for('createaccount'))
+        else:
+            session['username'] = user.username
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('accountHomePage'))
     return render_template('create-account.html', form=form)
 
 
@@ -100,6 +116,8 @@ def accountHomePage():
     Changelog:
         Dylan 5/3: Created initial implementation
     '''
-    u = User.query.filter_by(username=session['username']).first()
-    e = Event.query.filter_by(creator=u).all()
-    return render_template('meetings-page.html', events = e)
+    u=User.query.filter_by(username=session['username']).first()
+    e=Event.query.filter_by(creator=u).all()
+    if not e:
+        flash('No meetings on the horizon!')
+    return render_template('meetings-page.html', events=e)
