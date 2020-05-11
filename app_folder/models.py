06ecbc5bd44e92463c9b5746d0b58eed0c1635c3
dkiskamp <47258708+dkiskamp@users.noreply.github.com>
2020-05-11 16:52:31 -0700

    Guests can now schedule an event

--- app_folder/models.py
+++ app_folder/models.py
@@ -1,5 +1,5 @@
-from app_folder.__init__ import db
-from datetime import datetime
+from app_folder import db
+import datetime
 
 
 class User(db.Model):
@@ -12,11 +12,17 @@ class User(db.Model):
             4/19 Ali
             
             5/3 Dylan: Added events relationship
+            5/10 Dylan: Added salt
         """
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    salt = db.Column(db.String(32))
+    password_hash = db.Column(db.String(256), default = 'fatal-error')
+    start_time = db.Column(db.Time, default=datetime.time(9, 0))
+    end_time = db.Column(db.Time, default=datetime.time(22, 0))
+    meeting_length = db.Column(db.Integer, default=30)
+
 
     events = db.relationship('Event', backref='creator', lazy='dynamic')
     
